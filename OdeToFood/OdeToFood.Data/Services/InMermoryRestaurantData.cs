58aa3ec86737032cdfa9c2344dafb12a74bc9bfb
bhenning2556 <42550405+bhenning2556@users.noreply.github.com>
2021-07-21 12:15:13 -0700

    add restaurant data interface and some in memory data to services

---
+++ OdeToFood/OdeToFood.Data/Services/InMermoryRestaurantData.cs
@@ -0,0 +1,28 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMermoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMermoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                // This stores our sample data in memory. This would never be done in prod but is usefull for getting quick test data.
+                new Restaurant {Id = 1, Name = "Benny's Ramen Bar", Cuisine = CuisineType.Japanese },
+                new Restaurant {Id = 2, Name = "Benny's Pho Bar", Cuisine = CuisineType.Vietnamese },
+                new Restaurant {Id = 3, Name = "Benny's Express", Cuisine = CuisineType.Chinese },
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
